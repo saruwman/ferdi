be09e4a44b276ed0b9340d80b6eab829d8c103d5
Sara Hammouzou <sarahammouzou@gmail.com>
2020-05-05 15:27:35 +0000

    Change server select menu
    Replacing the input field with a select dropdown containing The default Ferdi server, Franz server, and custom option to input a custom server

--- src/components/auth/ChangeServer.js
+++ src/components/auth/ChangeServer.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import { defineMessages, intlShape } from 'react-intl';
-
+import { Badge } from '@meetfranz/ui';
 import Form from '../../lib/Form';
 import Input from '../ui/Input';
+import Select from '../ui/Select';
 import Button from '../ui/Button';
 
 const messages = defineMessages({
@@ -16,6 +17,11 @@ const messages = defineMessages({
     id: 'changeserver.label',
     defaultMessage: '!!!Server',
   },
+  warning: {
+    id: 'changeserver.warning',
+    defaultMessage: '!!!Some settings offered by Ferdi will not be saved',
+  },
+
   submit: {
     id: 'changeserver.submit',
     defaultMessage: '!!!Submit',
@@ -25,24 +31,29 @@ const messages = defineMessages({
 export default @observer class ChangeServer extends Component {
   static propTypes = {
     onSubmit: PropTypes.func.isRequired,
-    server: PropTypes.string.isRequired,
+    //server: PropTypes.string.isRequired,
   };
 
   static contextTypes = {
     intl: intlShape,
   };
 
+  ferdiServer='api.getferdi.com';
+
+  franzServer='this.props.server';
+
   form = new Form({
     fields: {
       server: {
         label: this.context.intl.formatMessage(messages.label),
-        value: '',
+        value: this.ferdiServer,
+        options: [{ value: this.ferdiServer, label: 'Ferdi server (Default)' }, { value: this.franzServer, label: 'Franz server' }, { value: '', label: 'Custom server' }],
       },
     },
   }, this.context.intl);
 
   componentDidMount() {
-    this.form.$('server').value = this.props.server;
+    // this.form.$('server').value = this.props.server;
   }
 
   submit(e) {
@@ -63,11 +74,20 @@ export default @observer class ChangeServer extends Component {
       <div className="auth__container">
         <form className="franz-form auth__form" onSubmit={e => this.submit(e)}>
           <h1>{intl.formatMessage(messages.headline)}</h1>
-
+          <Select field={form.$('server')} />
+          {![this.ferdiServer, this.franzServer].includes(form.$('server').value)
+          && (
           <Input
             field={form.$('server')}
             focus
           />
+          )}
+          {form.$('server').value === this.franzServer
+          && (
+          <Badge type="warning">
+            {intl.formatMessage(messages.warning)}
+          </Badge>
+          )}
           <Button
             type="submit"
             className="auth__button"
